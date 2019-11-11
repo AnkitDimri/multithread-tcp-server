3a49774c1022057761b4910187b439255b14d8aa
Ankit <ankitdimri1999@gmail.com>
2019-11-11 17:05:44 +0530

    first compiled client

--- client.cpp
+++ client.cpp
@@ -17,20 +17,42 @@
  };
 
  /* Signal handlre for ctrl+c to terminate the infinite client properly by closing the socket */
- void sig_handler(int signo) {
+ /*void sig_handler(int signo) {
 
-     /* signal handler */
+     /* signal handler *
      if (signo == SIGINT) {
          std::cout << "\t Exiting..." << '\n';
          close (fd);
          exit (1);
      }
- }
+ }*/
+
+ void func(int sockfd) {
+	msg buffer;
+	for (;;) {
+		memset (buffer.filename, 0, sizeof (buffer.filename));
+    memset (buffer.word, 0, sizeof (buffer.word));
+	  std::cout << "\n\t Enter the filename: ";
+		std::cin >> buffer.filename;
+    std::cout << "\n\t Enter the word: ";
+    std::cin >> buffer.word;
+		write (sockfd, &buffer, sizeof(buffer));
+
+    bool flag;
+		read(sockfd, &flag, sizeof(flag));
+		printf("From server :\n");
+
+    if (flag)
+      std::cout << "\n\t Word found!" << '\n';
+    else
+      std::cout << "\n\t Word not found!" << '\n';
+	}
+}
 
  int main(int argc, char const *argv[]) {
 
      int sockfd, connfd;
-	   struct sockaddr_in servaddr, cli ;
+	   sockaddr_in servaddr, cli ;
 
 	   // socket create and varification
 	   sockfd = socket(AF_INET, SOCK_STREAM, 0);
