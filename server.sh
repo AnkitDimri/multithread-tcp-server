2928ad4e8eac30433f0f452999fc86c35d777f30
Ankit <ankitdimri1999@gmail.com>
2019-11-24 22:57:24 +0530

    echo statements added

--- server.sh
+++ server.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+echo
+echo "Compiling server.cpp..."
 g++ server.cpp -o server -lpthread
+echo
+echo
+echo "server.cpp compiled..."
+echo
+echo "Starting server..."
+echo
 ./server
