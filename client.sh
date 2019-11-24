d4954dc32bffe1e5fba5859fe242ce73877cf0a9
Ankit <ankitdimri1999@gmail.com>
2019-11-24 23:01:34 +0530

    header added

--- client.sh
+++ client.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# @author: Ankit Dimri
+# Shell script to run the client which connects to server MISTCP
+
 # Compile client code and run
 echo
 echo "Compiling client.cpp..."
