67a7a912d6fbc654f9b28165a924282e4e1b6176
Ankit <ankitdimri1999@gmail.com>
2019-11-28 21:20:03 +0530

    indentation

--- server.sh
+++ server.sh
@@ -6,14 +6,17 @@
 # Compile server code and run
 echo
 echo "Compiling server.cpp..."
+
 # Compiling
 # lpthread flag to support multithreading
 g++ server.cpp -o server -lpthread
+
 echo
 echo
 echo "server.cpp compiled..."
 echo
 echo "Starting server..."
 echo
+
 # start the server
 ./server
